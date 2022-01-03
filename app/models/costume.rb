f5da58c11a51b0b4215c52f35a6cae1edece362e
troymeeker <troymeeker1@gmail.com>
2022-01-03 11:43:57 -0800

    commit tree 0008c8752fa52e884726b005cc3391822c74f155

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,15 @@
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
+class Costume < ActiveRecord::Base
+    def change
+        create_table :costumes do |t|
+          t.string :name
+          t.float :price
+          t.string :size
+          t.string :image_url
+          t.timestamp
+         end
+      end
+   
+
+end
--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,14 @@
 # Create your CostumeStore class here
+class CostumeStore < ActiveRecord::Base
+    def change
+        create_table :costume_stores do |t|
+            t.string :name
+            t.string :location
+            t.integer :costume_inventory
+            t.integer :num_of_employees
+            t.boolean :is_in_business
+            t.datetime :opening_time
+            t.datetime :closing_time
+        end
+    end
+end
